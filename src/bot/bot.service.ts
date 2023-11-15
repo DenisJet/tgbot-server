abd6576e5c757a2b11d281bd6b2d2d91ad55e3bd
Denis Kalkopf <deniskalkopf@gmail.com>
2023-11-15 10:27:59 +0500

    chat members reputation (wip)

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -19,5 +19,34 @@ export class BotService implements OnModuleInit {
         `Привет, ${msg.new_chat_members[0].first_name}! Это чат тестового приложения tgBot.`,
       ),
     );
+
+    bot.on('message', (msg) => {
+      if (msg?.sticker) {
+        if (msg.sticker.emoji === '👍') {
+          this.handleThanksWordReaction(msg, bot);
+        }
+      }
+    });
+  }
+
+  async handleThanksWordReaction(msg: TelegramBot.Message, bot: TelegramBot) {
+    const avatarUrl = await this.getUserAvatarUrl(
+      msg.reply_to_message.from.id,
+      bot,
+    );
+  }
+
+  async getUserAvatarUrl(userId: number, bot: TelegramBot) {
+    const userProfile = await bot.getUserProfilePhotos(userId);
+
+    if (!userProfile.photos.length) {
+      return '';
+    }
+
+    const fileId = userProfile.photos[0][0].file_id;
+    const file = await bot.getFile(fileId);
+    const filePath = file.file_path;
+
+    return `https://api.telegram.org/file/bot${process.env.BOT_API_TOKEN}/${filePath}`;
   }
 }
